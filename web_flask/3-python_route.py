87c6ffc7ca8ef0ea2c6b7501540b6a868f011761
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-05-17 12:17:49 +0300

    web_flask/3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """Return Hello HBNB on route(/)
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb/')
 def hbnb():
@@ -21,11 +23,13 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def describe_c(text):
     """Return C followed by text on route(/c/<text>)
     """
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
@@ -33,6 +37,7 @@ def describe_python(text="is cool"):
     """Return Python followed by text on route(/python/<text>)
     """
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
