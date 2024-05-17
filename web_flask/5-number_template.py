11fabd38fac5a1d81da747b714288019de14bb3f
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-05-17 12:19:41 +0300

    web_flask/5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """Return Hello HBNB on route(/)
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb/')
 def hbnb():
@@ -21,11 +23,13 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def describe_c(text):
     """Return C followed by text on route(/c/<text>)
     """
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
@@ -34,17 +38,20 @@ def describe_python(text="is cool"):
     """
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>')
 def number(n):
     """Return n is a number if n is a number on route(/python/<text>)
     """
     return f'{n} is a number'
 
+
 @app.route('/number_template/<int:n>')
 def number_template(n):
     """Return n is a number if n is a number on route(/python/<text>)
     """
     return render_template('5-number.html', number=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
