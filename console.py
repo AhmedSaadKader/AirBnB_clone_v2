b544680fd59c154f432dca806490afd14095dd54
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-05-17 12:05:30 +0300

    Airbnb

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Console Module """
+import shlex
 import cmd
 import sys
 from models.base_model import BaseModel
@@ -10,7 +11,6 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-import shlex
 
 
 class HBNBCommand(cmd.Cmd):
