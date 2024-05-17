ec300fb5f054ac21db9ce0cfb31d98fb2d96fb8d
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-05-17 12:14:33 +0300

    web_flask/0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """Return Hello HBNB on route(/)
     """
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
